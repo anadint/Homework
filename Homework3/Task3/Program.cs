9ab7c7992185deb9680e75bb2c001bce33cfefff
Talanova <ntalanov@its.jnj.com>
2023-07-27 00:31:22 +0300

    HW Task3

--- Homework3/Task3/Program.cs
+++ Homework3/Task3/Program.cs
@@ -2,3 +2,22 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void DefSqr(int num)
+{
+    Console.Write($"{num} -> ");
+    for (int i = 1; i < num; i++)
+    {
+        Console.Write(i * i * i + ", ");
+    }
+    Console.WriteLine(num * num * num + "."); 
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число: ");
+DefSqr(num);
