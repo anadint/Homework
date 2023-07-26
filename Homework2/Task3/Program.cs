f4a06736ddf5448f121f584fadac2f4a7c15cb51
Talanova <ntalanov@its.jnj.com>
2023-07-26 18:01:54 +0300

    HW Task3

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Result ( int arg)
+{
+    if (arg == 6 || arg == 7)
+    {
+        Console.WriteLine($"{arg} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{arg} -> нет");
+    }
+}
+
+Console.Write("Введите порядковый номер дня недели: ");
+int num = int.Parse(Console.ReadLine());
+
+Result(num);
