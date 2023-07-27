73af58e2d6718a9956b1ef7b747a2025aee18b32
Talanova <ntalanov@its.jnj.com>
2023-07-27 22:28:32 +0300

    HW4 Task2

--- Homework4/Task2/Program.cs
+++ Homework4/Task2/Program.cs
@@ -2,3 +2,25 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int GetSum(int num)
+{
+    int result = 0;
+    for (int i = 0; i < num; i++)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+    
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе {num} равна {GetSum(num)}");
