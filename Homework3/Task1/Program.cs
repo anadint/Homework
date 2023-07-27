1756bbd95e21c2932bc7f6bf39d3394f5258afec
Talanova <ntalanov@its.jnj.com>
2023-07-27 09:20:59 +0300

    HW Task1

--- Homework3/Task1/Program.cs
+++ Homework3/Task1/Program.cs
@@ -4,4 +4,50 @@
 // 12821 -> да
 // 23432 -> да
 
+void IsPalindrome(int num)
+{
+    if (num>9999 && num<100000)
+    {
+        if((num/10000 == num%10) && ((num/1000)%10 == (num%100)/10))
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это число не пятизначное");
+    }
+}
 
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите пятизначное число: ");
+IsPalindrome(num);
+
+// Console.Clear();
+// Console.WriteLine("Введите пятизначное число: ");
+// int A = int.Parse(Console.ReadLine());
+// if (A>9999 && A<100000)
+// {
+//    if((A/10000 == A%10) && ((A/1000)%10 == (A%100)/10))
+//    {
+//        Console.WriteLine("Да");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Нет");
+//    }
+// }
+// else
+// {
+//    Console.WriteLine("Это число не пятизначное");
+// }
