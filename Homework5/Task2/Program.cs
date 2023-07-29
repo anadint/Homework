73f6ae9d948c3f05136ac23d416d79a37e02cd35
Talanova <ntalanov@its.jnj.com>
2023-07-29 11:34:02 +0300

    HW Task2

--- Homework5/Task2/Program.cs
+++ Homework5/Task2/Program.cs
@@ -1,3 +1,28 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int FindCount (int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1) 
+        result += array[i];
+    }
+    return result;
+}
+
+int[] GenArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i <= length - 1; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int[] array = GenArray(4, -99, 100);
+int count = FindCount(array);
+Console.Write("[" + string.Join(",", array) + "] -> " + FindCount(array));
