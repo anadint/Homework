954f000e05c8050ea4cf7f705025f7fdf4a7822d
Talanova <ntalanov@its.jnj.com>
2023-07-31 01:57:43 +0300

    HW7 Task1

---
+++ Homework7/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void PrintDoubleMassive(double [,] massive)
+{
+     for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] DoDoubleMassive(int rows, int columns, double minValue, double maxValue)
+{
+    double [,] result = new double [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+          result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+          result[i, j] = Math.Round(result[i, j],2); 
+        }
+    }
+    return result;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int rows = GetInfo("Введите количество строк: ");
+int columns = GetInfo("Введите количество столбцов: ");
+double [,] massive = DoDoubleMassive(rows, columns, -9, 9);
+PrintDoubleMassive(massive);
