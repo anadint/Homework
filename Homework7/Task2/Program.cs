bfe331a4878afb01d7af23218f03c99d7d40df11
Talanova <ntalanov@its.jnj.com>
2023-07-31 17:03:15 +0300

    HW7 Task2

---
+++ Homework7/Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void GetCheck(int[,] array, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                count++;
+            }
+        }
+    }
+    if (count > 0)
+    {     
+        Console.WriteLine($"Число {num} встречается в массиве {count} раза");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> такого числа в массиве нет");   
+    }
+}
+
+void PrintArray(int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+          result[i, j] = new Random().Next(minValue, maxValue + 1);  
+        }
+    }
+    return result;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int rows = GetInfo("Введите количество строк: ");
+int columns = GetInfo("Введите количество столбцов: ");
+int num = GetInfo("Введите число для проверки: ");
+int[,] array = FillArray(rows, columns, 0, 9);
+PrintArray(array);
+GetCheck(array, num);
