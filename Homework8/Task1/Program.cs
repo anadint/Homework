6685f627f43540457a1ae3872c13a4337bfb9683
Talanova <ntalanov@its.jnj.com>
2023-08-10 23:43:43 +0300

    HW8 Task1

--- Homework8/Task1/Program.cs
+++ Homework8/Task1/Program.cs
@@ -7,3 +7,52 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 100);
+PrintArray(array);
+Console.WriteLine();
+SortOfLines(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortOfLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            for (int tmp = 0; tmp < array.GetLength(1) - 1; tmp++){
+                if (array[i, tmp] < array[i, tmp + 1]){
+                    int temp = array[i, tmp + 1];
+                    array[i, tmp + 1] = array[i, tmp];
+                    array[i, tmp] = temp;
+                }
+            }
+        }
+    }
+}
