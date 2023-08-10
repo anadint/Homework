6e4a8b8fe47a361f4a7c96f01b2a79f07184ccf7
Talanova <ntalanov@its.jnj.com>
2023-08-10 23:50:38 +0300

    HW8 Task3

--- Homework8/Task3/Program.cs
+++ Homework8/Task3/Program.cs
@@ -5,3 +5,56 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+Console.Clear();
+
+int[,] A = GetArray(2, 2, 1, 9);
+int[,] B = GetArray(2, 2, 1, 9);
+PrintArray(A);
+Console.WriteLine();
+PrintArray(B);
+Console.WriteLine();
+PrintArray(GetProductOfArray(A,B));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetProductOfArray(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] = arrayC[i, j] + arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
